17b5b39b8a71b6991d9a4b5ac6ef22c07b6c0c8a
Ingrid Alkimim <ingridalkm@outlook.com>
2024-06-26 16:45:21 -0300

    up

---
+++ src/page/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <p>A comunicação e o trabalho em equipe são essenciais</p>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
